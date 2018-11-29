22bb972230b0956686b76612810622850893cf8e
NeonDragon1909 <nahil123@live.com>
2018-11-29 11:45:46 +0500

    fix indentation

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from telethon import TelegramClient, events, sync
 import data
 
 with TelegramClient('userbot', data.api_id, data.api_hash) as client:
-	client.start()
-    print(client.get_me().stringify())
+  client.start()
+  print(client.get_me().stringify())
